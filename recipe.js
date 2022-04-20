1a5ee1c0a648be67f10551b9422169cce7655e58
martin <omoshalego@gmail.com>
2022-04-20 04:03:54 +0100

    added nutrition key

--- recipe.js
+++ recipe.js
@@ -61,13 +61,13 @@ function updateRecipe(){
                         
                         response.recipe[index].Serves +
                         response.recipe[index].Easy +
+                       
                         response.recipe[index].Ingredients[index]+
+                        
                         response.recipe[index].Method[index]
-
                     });
                     $("#recipe").append( recipeinfo);
                     updateRecipe();
-
                 },
                 error:function(){
                     $("#info").html("<p>An error has occured</p>");
@@ -76,3 +76,43 @@ function updateRecipe(){
         }, 250);
     }}) ();
 
+    var barColor =7;
+    function myNutrition(){
+   const ctx = document.getElementById('myChart').getContext('2d');
+   const myChart = new Chart(ctx, {
+       type: 'bar',
+       data: {
+           labels: ['KCAL', 'FAT', 'SATURATE', 'CARBS', 'SUGARS', 'FIBRE','PROTEIN','SALT'],
+           datasets: [{
+               label: 'NUTRITION: per serving in g',
+               data: [12, 19, 3, 5, 2, 3],
+               backgroundColor: [
+                   'rgba(255, 99, 132, barColor)',
+                   'rgba(54, 162, 235, barColor)',
+                   'rgba(255, 206, 86, barColor)',
+                   'rgba(75, 192, 192, barColor)',
+                   'rgba(153, 102, 255, barColor)',
+                   'rgba(255, 159, 64, barColor)'
+               ],
+               borderColor: [
+                   'rgba(255, 99, 132, 1)',
+                   'rgba(54, 162, 235, 1)',
+                   'rgba(255, 206, 86, 1)',
+                   'rgba(75, 192, 192, 1)',
+                   'rgba(153, 102, 255, 1)',
+                   'rgba(255, 159, 64, 1)'
+               ],
+               borderWidth: 1
+           }]
+       },
+       options: {
+           scales: {
+               y: {
+                   beginAtZero: true
+               }
+           }
+       }
+   });
+    }
+   
+    document.addEventListener('DOMContentLoaded', myNutrition);
