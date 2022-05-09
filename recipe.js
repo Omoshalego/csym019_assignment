7b8c4f1ee1a5b54cf958775f729b0394c2bc6d99
MOmondi <45461108+Omoshalego@users.noreply.github.com>
2022-05-09 21:49:21 +0100

    inseting data to json file

--- recipe.js
+++ recipe.js
@@ -1,20 +1,131 @@
 
+// let $js_dom_array = [];
+// let $js_am_label_arr = ["kcal", "fat","saturate","carbs","sugar","fibre","protein","salt"];
+// var chart;
+
+
+//$(document).ready(
+
+
+// function updateRecipe(){
+//     setTimeout(
+//         function(){
+//             $.ajax({
+//                 url:"recipe.json",
+//                 type:"GET",
+//                 dataType:"json",
+//                 success:function(response){
+                    
+//                     let recipeinfo = ""; //Variable to hold the content of the recipe
+//                     let methods ="";     // Variable to hold the content of the method
+
+//                     $("#recipe").html("");  
+//                     $("#method").html("");  
+
+//                     $.each(response.recipe,function(index,item){ 
+//                         var ingredientList = item.Ingredients; 
+//                         var ingredientOrder ="";
+                       
+//                         var chartList = item.Nutrition_per_serve;
+//                          var chartOrder ="";
+
+//                         var arrayOfIngredient=[];
+
+//                         var methodList = item.Method; 
+//                         var methodOrder ="";
+
+//                         var arrayOfMethod=[];
+                       
+
+//                          $.each(chartList, function(i, eachChart) {
+//                              chartOrder =eachChart;                           
+//                              $js_dom_array +=  chartOrder;
+//                               });
+
+//                         $.each(ingredientList, function(i, eachIngredient) {
+//                             ingredientOrder =eachIngredient;                           
+//                             arrayOfIngredient += "<tr><td>" + [i+1] +")  "+ ingredientOrder+"</tr></td>";
+//                              });
+
+//                        $.each(methodList, function(i, eachMethod) {
+//                                 methodOrder =eachMethod;                           
+//                                 arrayOfMethod +=  "<tr><td><strong>Step: " +[i +1]  +
+//                                   "<strong></td></tr><tr><td>"+ methodOrder+"</td></tr>";
+//                                  });
+                        
+//                       methods += "<tr><td><h3>Method </h3></td></tr>"+ arrayOfMethod;
+
+//                       recipeinfo +="<tr class ='recipes'><td><h3>" +
+
+//                       response.recipe[index].title + "</h3></td><td>"+
+                      
+//                     //   response.recipe[index].Image + "</td></tr>" +
+                     
+
+//                       "<tr><td>"+
+
+//                    "Prep: "+ response.recipe[index].prep + " mins" +
+                   
+//                      "\t\tCook: "+response.recipe[index].Cook + " mins" + 
+                     
+//                      "\t\tServes: "+response.recipe[index].Serves + 
+//                      "\t\tEasy: "+ response.recipe[index].Easy  +
+//                      "</td></tr>" +
+
+//                      "<tr><td><h3>Ingredient </h3></td></tr>" + arrayOfIngredient 
+           
+//                     });
+//                    // $("#myChart").append( myCharts);
+//                     $("#method").append( methods);
+//                     $("#recipe").append( recipeinfo);
+//                     updateRecipe();
+//                 },
+//                 error:function(){
+//                     $("#info").html("<p>An error has occured</p>");
+//                 }
+//             });
+//         }, 250);
+//     },
+    
+
+
+    //  START SERVER COMMAND   $ docker run -p 80:80 -v //C/CSYM019_ASSIGNMENT:/var/www/html -d muawya/csym019:websvr1.0            
+
+var chart;
+let $js_dom_array =[];
+let $js_am_label_arr =[];
+var itemTittle;
+
 $(document).ready(
-function updateRecipe(){
+  
+  function updateRecipe(){
+    
+    $js_dom_array = [10,20,30,40,50,60,70,80 ];
+    $js_am_label_arr = ["kcal", "fat","saturate","carbs","sugar","fibre","protein","salt"];
+   // chartFunction();
     setTimeout(
         function(){
             $.ajax({
+             
                 url:"recipe.json",
                 type:"GET",
                 dataType:"json",
                 success:function(response){
-                    
-                    let recipeinfo = "";
-                    let methods ="";
-                    $("#recipe").html("");                  
+  
+                 
+                    let recipeinfo = ""; //Variable to hold the content of the recipe
+                    let methods ="";     // Variable to hold the content of the method
+
+                    $("#recipe").html("");  
+                    $("#method").html("");  
+
                     $.each(response.recipe,function(index,item){ 
                         var ingredientList = item.Ingredients; 
                         var ingredientOrder ="";
+                       
+                        var chartList = item.Nutrition_per_serve;
+                         var chartOrder ="";
+                         itemTittle =response.recipe[index].title;
 
                         var arrayOfIngredient=[];
 
@@ -22,22 +133,30 @@ function updateRecipe(){
                         var methodOrder ="";
 
                         var arrayOfMethod=[];
+                       
+
+                         $.each(chartList, function(i, eachChart) {
+                             chartOrder =eachChart;                           
+                             $js_dom_array +=  chartOrder;
+                        
+                              });
 
                         $.each(ingredientList, function(i, eachIngredient) {
                             ingredientOrder =eachIngredient;                           
                             arrayOfIngredient += "<tr><td>" + [i+1] +")  "+ ingredientOrder+"</tr></td>";
                              });
 
-                             $.each(methodList, function(i, eachMethod) {
+                       $.each(methodList, function(i, eachMethod) {
                                 methodOrder =eachMethod;                           
                                 arrayOfMethod +=  "<tr><td><strong>Step: " +[i +1]  +
-                                  "<strong></td></tr><tr><td>"+ methodOrder+"</td></tr><br>";
+                                  "<strong></td></tr><tr><td>"+ methodOrder+"</td></tr>";
                                  });
                         
                       methods += "<tr><td><h3>Method </h3></td></tr>"+ arrayOfMethod;
-                      recipeinfo +="<tr class ='recipes'><td><h2>" +
 
-                      response.recipe[index].title + "</h2></td><td>"+
+                      recipeinfo +="<tr class ='recipes'><td><h3>" +
+
+                      response.recipe[index].title + "</h3></td><td>"+
                       
                     //   response.recipe[index].Image + "</td></tr>" +
                      
@@ -52,99 +171,55 @@ function updateRecipe(){
                      "\t\tEasy: "+ response.recipe[index].Easy  +
                      "</td></tr>" +
 
-                     "<tr><td><h3>Ingredient </h3></td></tr>" + arrayOfIngredient 
-
-                    //  "<tr><td align='center' colspan='3'><h3>Method<h3></td></tr>"
-                    //  + arrayOfMethod                
+                     "<tr><td><h3>Ingredient </h3></td></tr>" + arrayOfIngredient
+                   
                     });
-                    $("#method").append( methods);
+                  
+                     $("#method").append( methods);
                     $("#recipe").append( recipeinfo);
                     updateRecipe();
+                   // chartFunction()
+
                 },
                 error:function(){
                     $("#info").html("<p>An error has occured</p>");
                 }
             });
-        }, 250);
-    }) ();
+     
+          },2250);
+          chartFunction();
+      
+      })();
+
+  
+  function chartFunction() {
+  chart = new Highcharts.Chart({
+    chart: {
+      renderTo: 'chart_container', //under chart_container chart will display
+      defaultSeriesType: 'bar', //bar grapgh
+      backgroundColor: '#CCCCBB',
+      type: 'column' ,//to display in columns wise
+
+      
+      bar: {
+        colorByPoint: true,
+        dataLabels: {
+          enabled: false
+        }
+      }
+    },
+    title: {
+      text: 'Nutrition_per_serve in '  + itemTittle
+    },
+    xAxis: {
+      categories: $js_am_label_arr, //for value in labels
+    },
+    series: [{
+      name: 'Nutrients in grams',
+      data: $js_dom_array //array value to plot data
+    }]
+
+  });
+}
 
 
-//LOADING THE PAGE USING WINDOWS
-/*$( window ).on( "load",
-function updateRecipe(){
-    setTimeout(
-        function(){
-            $.ajax({
-                url:"recipe.json",
-                type:"GET",
-                dataType:"json",
-                success:function(response){
-                    let recipeinfo = "";
-                    $("#recipe").html("");
-                    $.each(response.recipe,function(index){
-                        recipeinfo +="<tr class = 'score'><td>" +
-                        response.recipe[index].title + 
-                        response.recipe[index].prep + "<td><td>" +
-                        response.recipe[index].Cook + "<td><td>" +
-                        "</td></tr><tr><td>" +
-                        response.recipe[index].Serves +
-                        "</td></tr><tr><td>" +
-                        response.recipe[index].Easy +
-                        "</td></tr><tr><td>" +
-                        response.recipe[index].Ingredients[index]+
-                        "</td></tr><tr><td>" +
-                        response.recipe[index].Method[index]
-                        "</td><td></td><td></td></tr>";
-                    });
-                    $("#recipe").append( recipeinfo);
-                    updateRecipe();
-                },
-                error:function(){
-                    $("#info").html("<p>An error has occured</p>");
-                }
-            });
-        }, 250);
-    }) ();*/
-
-
-    
-//     var barColor =8;
-//     function myNutrition(){
-//    const ctx = document.getElementById('myChart').getContext('2d');
-//    const myChart = new Chart(ctx, {
-//        type: 'bar',
-//        data: {
-//            labels: ['KCAL', 'FAT', 'SATURATE', 'CARBS', 'SUGARS', 'FIBRE','PROTEIN','SALT'],
-//            datasets: [{
-//                label: 'NUTRITION: per serving in g',
-//                data: [12, 19, 3, 5, 2, 3],
-//                backgroundColor: [
-//                    'rgba(255, 99, 132, barColor)',
-//                    'rgba(54, 162, 235, barColor)',
-//                    'rgba(255, 206, 86, barColor)',
-//                    'rgba(255, 206, 86, barColor)',
-//                    'rgba(75, 192, 192, barColor)',
-//                    'rgba(153, 102, 255, barColor)',
-//                    'rgba(255, 159, 64, barColor)'
-//                ],
-//                borderColor: [
-//                    'rgba(255, 99, 132, 1)',
-//                    'rgba(54, 162, 235, 1)',
-//                    'rgba(255, 206, 86, 1)',
-//                    'rgba(75, 192, 192, 1)',
-//                    'rgba(153, 102, 255, 1)',
-//                    'rgba(255, 159, 64, 1)'
-//                ],
-//                borderWidth: 1
-//            }]
-//        },
-//        options: {
-//            scales: {
-//                y: {
-//                    beginAtZero: true
-//                }
-//            }
-//        }
-//    });
-//     }
-   
