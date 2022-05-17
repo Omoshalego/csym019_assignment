149e5810446830dfbb5f6160b70afb75f0585644
MOmondi <45461108+Omoshalego@users.noreply.github.com>
2022-05-17 02:38:47 +0100

    Adding  nutrition row

--- recipes.js
+++ recipes.js
@@ -26,7 +26,8 @@
               var nutritionOrder ="";                // Variable to hold each nutritiont list.
               var nutritionVal =item.Nutrition;      //Variable to hold nutrtion object array.           
               var arrayOfIngredient=[];              //Declearing array variable to hold ingredient array. 
-              var arrayOfMethod=[];                  //Declearing array variable to hold method array.
+              var arrayOfMethod=[]; 
+              var arrayOfNutrition=[];                   //Declearing array variable to hold method array.
 
               var methodOrder ="";                    // Variable to hold each method list.        
                $.each(ingredientList, function(i, eachIngredient) { //This function loop through all the ingredient values content in recipe json file.
@@ -34,6 +35,7 @@
                  arrayOfIngredient += "<tr><td>" + [i+1] +")  "+ //Printing the ingredients list in a table format.
                  ingredientOrder+"</td></tr>";
                     });
+
                $.each(methodList, function(i, eachMethod) {   //Looping through the list of method to print individual method
                  methodOrder =eachMethod;                           
                  arrayOfMethod +=  "<tr><td><strong>Step: " +[i +1]  +   //Assigning  the method step in a recipe
@@ -41,23 +43,28 @@
                  methodOrder+"</td></tr><br>";           //Printing the method array
                    });         
                  methods += "<tr><td><h3>Method </h3></td></tr>"+ arrayOfMethod;  
-                        
-                //  nutritionOrder +=                  //Printing the various nutrition values
-                //   "<tr><td><h3>Nutritoin Per Serving </h3></td></tr>" +                             
-                //     "<tr>"+                   
-                //     "<td> <em>kcal: " +nutritionVal[index].kcal+"g "+                                
-                //     "\t\t\t\tfat: " + nutritionVal[index].fat+   "g"+
-                //     "\t\t\t\t saturate: " + nutritionVal[index].saturate+   "g"+
-                //     "\t\t\t\tCarbs: " + nutritionVal[index].carbs+   "g"+                  
-                //     "\t\t\t\tsugars: " + nutritionVal[index].sugars+   "g"+
-                //     "\t\t\t\tFibre: " + nutritionVal[index].fibre+   "g"+                  
-                //     "\t\t\t\tProtein: " + nutritionVal[index].protein+   "g"+
-                //     "\t\t\t\tSalt: " + nutritionVal[index].salt+   " g</em></td></tr>";
+  
+                 $.each(nutritionVal, function(i) { 
+                   
+                 nutritionOrder +=                  //Printing the various nutrition values
+                  // "<tr><td><h3>Nutritoin Per Serving </h3></td></tr>" +                             
+                    "<tr>"+                   
+                    "<td> <em>kcal: " +nutritionVal[i].kcal+"g "+                                
+                    "\t\t\t\tfat: " + nutritionVal[i].fat+   "g"+
+                    "\t\t\t\t saturate: " + nutritionVal[i].saturate+   "g"+
+                    "\t\t\t\tCarbs: " + nutritionVal[i].carbs+   "g"+                  
+                    "\t\t\t\tsugars: " + nutritionVal[i].sugars+   "g"+
+                    "\t\t\t\tFibre: " + nutritionVal[i].fibre+   "g"+                  
+                    "\t\t\t\tProtein: " + nutritionVal[i].protein+   "g"+
+                    "\t\t\t\tSalt: " + nutritionVal[i].salt+   " g</em></td></tr>";
+               arrayOfNutrition += "<tr><td><h3>Nutritoin Per Serving </h3></td></tr>" + nutritionOrder;
+                    
+                  });
+
 
                  recipeinfo +=
                     "<tr class ='recipes'><td><h2>" +
                      response.recipe[index].title + 
-                     "</h2></td><td>"+                        
                        "<img src='" + response.recipe[index].Image + "'/></td></tr>" +
                        "<tr><td>"+  
                        "Prep: "+ response.recipe[index].prep + " mins" +   
@@ -66,7 +73,7 @@
                         "\t\tEasy: "+ response.recipe[index].Easy  +
                         "</td></tr>" +
                          "<tr><td><h3>Ingredient </h3></td></tr>" +
-                         arrayOfIngredient 
+                         arrayOfIngredient + arrayOfNutrition
                         // nutritionOrder                          
                         });
                                              
